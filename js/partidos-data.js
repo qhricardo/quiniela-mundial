840b6c5b7eca44b152a23404886e8c3338441fdf
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-06-09 13:22:45 -0600

    Update partidos-data.js

--- js/partidos-data.js
+++ js/partidos-data.js
@@ -1,12 +1,12 @@
 window.partidosProgramados = [
-  "P1 Sabado, 14/06/2025 18:15hrs: Mexico 0vs Republica Dominicana 0",
-  "P2 Sabado, 14/06/2025 16:00hrs: Al Ahly CF vs Inter Miami CF",
-  "P3 Domingo 15/06/2025 16:00hrs: Estados Unidos vs Trinidad y Tobago",
-  "P4 Domingo 15/06/2025 18:15hrs: Haití  vs Arabia Saudí",
-  "P5 Domingo 15/06/2025 21:00hrs: Costa Rica vs Surinam ",
-  "P6 Domingo 15/06/2025 13:00hrs: PSG   vs At. Madrid",
-  "P7 Domingo 15/06/2025 16:00hrs: Palmeiras  vs FC Porto",
-  "P8 Domingo 15/06/2025 20:00hrs: Botafogo  vs Seattle Sounders ",
-  "P9 Lunes 16/06/2025 13:00hrs: Chelsea  vs Los Angeles FC ",
-  "P10 Lunes 16/06/2025 20:00hrs: Jamaica  vs Guatemala "
+  "P1 Mexico 0vs Republica Dominicana 0",
+  "P2 Al Ahly CF vs Inter Miami CF",
+  "P3 Estados Unidos vs Trinidad y Tobago",
+  "P4 Haití  vs Arabia Saudí",
+  "P5 Costa Rica vs Surinam ",
+  "P6 PSG   vs At. Madrid",
+  "P7 Palmeiras  vs FC Porto",
+  "P8 Botafogo  vs Seattle Sounders ",
+  "P9 Chelsea  vs Los Angeles FC ",
+  "P10 Jamaica  vs Guatemala "
 ];
